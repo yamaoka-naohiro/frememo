ceb8c65bafd2c039e6267e94a7ecc6819cc2ce03
Naohiro Yamaoka <naohiro@Naohiro-no-MacBook-Air.local>
2018-04-05 20:23:06 +0900

    memos controller 作成

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,3 @@ end  # The priority is based upon order of creation: first created -> highest pr
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
